de4ce906a3e9839224302bef5a9f4c73d8e1a584
rzamora2 <rzamora@devtch.com>
2022-10-15 11:42:18 -0600

    Added: Login functionality

--- bancosangreapp/src/screens/Login.js
+++ bancosangreapp/src/screens/Login.js
@@ -1,9 +1,42 @@
-import * as React from 'react';
-import { Button, View, Text, StyleSheet,Image, TextInput, TouchableHighlight } from 'react-native';
+import React, { useState } from 'react';
+import { Button, View, Text, StyleSheet, Image, TextInput, TouchableHighlight, Alert } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import customConfig from '../../custom-config.json';
 
-export const Login = ({ navigation }) => {  
+export const Login = ({ navigation }) => {
+  const [nombreUsuario, setNombreUsuario] = useState("");
+  const [pwd, setPwd] = useState("");
+
+  const login = async () => {
+    const url =
+      customConfig.apiURL + "Usuarios/Login?" + new URLSearchParams({
+        NombreUsuario: nombreUsuario,
+        Pwd: pwd,
+      });
+    try {
+      var responseJ;
+      await fetch(url)
+        .then(async function (response) {
+          if(response.status == 200){ //finded
+            responseJ = await response.json();
+            navigation.navigate('Menu usuario')
+          }
+          else if(response.status == 500){ //connection lost
+            Alert.alert('Error', 'Intente de nuevo');
+          }
+          else{ //error
+            Alert.alert('Error', 'Credenciales inválidas');
+          }
+        }).then(function (data) {
+          console.log(data);
+        });
+      console.log(responseJ);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
     <>
       <Text style={styles.titulo1}>Banco de sangre</Text>
@@ -13,11 +46,12 @@ export const Login = ({ navigation }) => {
       <Text style={styles.titulo2}>¡Bienvenido!</Text>
       <View style={styles.cards}>
         <Text style={styles.cardTitle}>Usuario:</Text>
-        <TextInput 
+        <TextInput
           keyboardType='default'
           placeholder='Ingrese su usuario'
           placeholderTextColor={'white'}
           style={styles.cardText}
+          onChangeText={(value) => setNombreUsuario(value)}
         />
         <Text style={styles.cardTitle}>Contraseña:</Text>
         <TextInput
@@ -26,19 +60,20 @@ export const Login = ({ navigation }) => {
           placeholderTextColor={'white'}
           secureTextEntry={true}
           style={styles.cardText}
+          onChangeText={(value) => setPwd(value)}
         />
         <TouchableHighlight
-          onPress={() => {navigation.navigate('Menu usuario')}}
+          onPress={() => { login() }}
         >
           <View style={styles.buttonContainer}>
             <Text style={styles.button}>Ingresar</Text>
           </View>
         </TouchableHighlight>
       </View>
-      <View style={{  alignItems: 'center', justifyContent: 'center'}}>
-			  <Button
-        title="¿Olvidó su contraseña?"
-        onPress={() => navigation.navigate('Recuperacion')}
+      <View style={{ alignItems: 'center', justifyContent: 'center' }}>
+        <Button
+          title="¿Olvidó su contraseña?"
+          onPress={() => navigation.navigate('Recuperacion')}
         />
       </View>
     </>
@@ -47,35 +82,35 @@ export const Login = ({ navigation }) => {
 }
 
 const styles = StyleSheet.create({
-  imgContainer:{    
+  imgContainer: {
     alignItems: 'center',
-    justifyContent: 'center', 
-    marginTop: 10 
+    justifyContent: 'center',
+    marginTop: 10
   },
   img: {
     height: 150,
     width: 150,
-    marginBottom:10   
+    marginBottom: 10
   },
-  titulo1:{
-    fontWeight:'bold',
-    fontSize:35,
-    textAlign:'center',
-    marginTop:10,
+  titulo1: {
+    fontWeight: 'bold',
+    fontSize: 35,
+    textAlign: 'center',
+    marginTop: 10,
   },
-  titulo2:{
-    fontWeight:'bold',
-    fontSize:25,
-    textAlign:'center',
-    marginTop:10,
-    marginBottom:5,
+  titulo2: {
+    fontWeight: 'bold',
+    fontSize: 25,
+    textAlign: 'center',
+    marginTop: 10,
+    marginBottom: 5,
   },
-  cards:{
+  cards: {
     backgroundColor: '#C43B58',
     height: 255,
-    width:'97.5%',
+    width: '97.5%',
     margin: 5,
-    borderRadius: 14,    
+    borderRadius: 14,
     alignItems: 'center',
     shadowRadius: 2,
     shadowColor: 'black',
@@ -88,31 +123,31 @@ const styles = StyleSheet.create({
     shadowRadius: 11.14,
     elevation: 17,
   },
-  cardTitle:{
+  cardTitle: {
     fontSize: 25,
     color: '#ffffff',
     marginTop: 10
   },
-  cardText:{
+  cardText: {
     height: 40,
     width: 350,
     marginTop: 15,
     borderWidth: 2,
     padding: 10,
     color: 'white',
-    fontSize: 15,  
+    fontSize: 15,
     borderRadius: 10,
-    borderColor: 'white'  
+    borderColor: 'white'
   },
-  button:{
-    fontSize:20,
-    color:'#C43B58',
-    fontWeight:'bold',
-    padding:8,
+  button: {
+    fontSize: 20,
+    color: '#C43B58',
+    fontWeight: 'bold',
+    padding: 8,
   },
-  buttonContainer:{
-    backgroundColor:'white',
-    marginTop:10,
-    borderRadius:2
+  buttonContainer: {
+    backgroundColor: 'white',
+    marginTop: 10,
+    borderRadius: 2
   },
 });
